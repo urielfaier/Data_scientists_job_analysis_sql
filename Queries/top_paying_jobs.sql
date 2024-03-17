f94c41b39146efa31de775e08c0bdb8e90ba26a3
Uriel Faier <urielfaier@gmail.com>
2024-03-17 14:28:19 +0200

    first commit

---
+++ Queries/top_paying_jobs.sql
@@ -0,0 +1,10 @@
+
+/*
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely
+- Focuses on job postings with specified salaries (remove nulls)
+- BONUS: Include company names of top 10 roles
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
+*/
+
+-- commit one 
